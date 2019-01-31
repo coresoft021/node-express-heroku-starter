4133e212263cf522939740a5eb6b1638500ff99c
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-01-31 20:25:30 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,7 @@ const express = require('express');
 import { json, urlencoded } from "body-parser";
 import { Request, Response, Router } from "express";
 import { publicRouter } from "./routes/public";
-import { dbRouter } from "./routes/cdb";
-import { userRouter } from "./routes/user";
-import { testRouter } from "./routes/test";
+
 var cors = require('cors');
 const app = express();
 const path = require('path');
@@ -15,9 +13,7 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use("/api/public", publicRouter);
-app.use("/api/cdb", dbRouter);
-app.use("/api/user", userRouter);
-app.use("/api/test", testRouter);
+
 
 
 app.get('/api', function (req, res) {
