a29978eda800e343a73fcd0bdec0b6b046d2c7cf
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-02-06 22:02:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -10,14 +7,8 @@ const app = express();
 //set the path to the public folder for static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
-//set the path to the views folder
-app.set('views', path.join(__dirname, 'views'));
 
-//use express-handlebars as the views engine
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+
 
 
 app.get('/', (req,res) => {
